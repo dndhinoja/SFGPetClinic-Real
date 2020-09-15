f252e599b6c41d225d6ecfc702d83f7029bfaba9
dndhinoja <dev.dipali.dhinoja@gmail.com>
2020-09-15 12:48:05 +0530

    added Specility Map Impl, closes #32

---
+++ pet-clinic-data/src/main/java/services/Map/SpecialitiesMapService.java
@@ -0,0 +1,38 @@
+package services.Map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import model.Speciality;
+import services.SpecialitiesService;
+
+@Service
+public class SpecialitiesMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService{
+
+	@Override
+	public Set<Speciality> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Speciality save(Speciality object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void delete(Speciality object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+}
